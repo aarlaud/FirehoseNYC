2050d0a3656fdecf1a16fed770ce72d5ce727ded
Antoine Arlaud <aarlaud@hotmail.com>
2013-10-20 15:18:01 -0400

    Added User Authentication using devise gem. Styling almost complete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,10 @@ Testapp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Devise Gem default url
+  config.action_mailer.default_url_options = {
+    :host => 'firehosenycaarlaud.herokuapp.com'
+  }
+
 end
