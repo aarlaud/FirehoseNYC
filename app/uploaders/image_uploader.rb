4a55d0f1c3440e56fdc58589072b6be936d22124
Antoine Arlaud <aarlaud@hotmail.com>
2013-10-20 11:54:59 -0400

    Fixing Heroku persistence by uploading picture to Amazon S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # The carrierwave.rb file in initializer defines the storage rules
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
